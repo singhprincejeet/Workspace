8c0f09b61728bf44ea076f37c935875da846410f
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2015-04-27 13:48:46 -0400

    Solution to problem number 2

--- Euler'sProblem/src/Problem2.java
+++ Euler'sProblem/src/Problem2.java
@@ -4,17 +4,21 @@
 public class Problem2 {
     public static void main(String[] args) {
         int a1=1,a2=2;
-        int sum = 0;
-        for (int i = 0; i <100;i++) {
-            int a3=a1+a2;
+        int sum = 0,a3=0;
+        if (a1%2==0){
+            sum+=a1;
+        }
+        if (a2%2==0){
+            sum+=a2;
+        }
+        for (int i = 0; i < 4000000 && a3<4000000; i++) {
+            a3= a1+a2;
             a1=a2;
             a2=a3;
-            System.out.println(a3);
-//            if (i%3==0 || i%5==0)
-//                sum+=i;
+            if (a3%2==0) {
+                sum += a3;
+            }
         }
-
-//        System.out.println(sum);
-
+        System.out.println(sum);
     }
 }
