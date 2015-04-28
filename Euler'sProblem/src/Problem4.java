3e3235cb7e09e7fcf934956ef2a9843b4421c8b6
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2015-04-28 09:06:08 -0400

    Solution to Problem number 4

---
+++ Euler'sProblem/src/Problem4.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by prince on 4/28/15.
+ */
+public class Problem4 {
+    public static void main(String[] args) {
+        int largestPallindrome=0;
+        for (int i = 999 ; i> 100; i--){
+            for (int j = 999; j > 100; j--) {
+                int product = i*j;
+                if (isPalindrome(product)) {
+                    if (product>largestPallindrome)
+                        largestPallindrome=product;
+                }
+            }
+        }
+        System.out.println(largestPallindrome);
+    }
+
+    private static boolean isPalindrome(int i) {
+        String s = ""+i, temp ="";
+        for (i = s.length()-1; i>=0;i--){
+            temp=temp+s.charAt(i);
+        }
+        return temp.equals(s);
+    }
+}
