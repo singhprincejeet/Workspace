1a648c1e1285f9510c3030417fe7adcdbc4f3ff2
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2015-04-28 09:12:06 -0400

    modified

--- Euler'sProblem/src/Problem3.java
+++ Euler'sProblem/src/Problem3.java
@@ -5,29 +5,18 @@ public class Problem3 {
 
     public static void main(String[] args)
     {
-
         long num = 600851475143L;
-        long prime = 2;
-        while (true)
-        {
-            long tmp = 600851475143L / prime;
-            if ( num % tmp == 0 &&  isPrimeNumber(tmp) )
-            {
-                System.out.println(tmp);
-                break;
-            }
-            prime++;
-        }
-    }
-
-    public static boolean isPrimeNumber(long number){
-
-        for(long i=2; i<=number/2; i++){
-            if(number % i == 0){
-                return false;
+        long largestPrime = 0L;
+        for(long i = 2; i<=num/i;i++){
+            while(num%i==0){
+                largestPrime = i;
+                num /=i;
             }
         }
-        return true;
+        if(num>1){
+            largestPrime = num;
+        }
+        System.out.println(largestPrime);
     }
 
 }
