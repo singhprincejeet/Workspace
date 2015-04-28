7e910734a7486b0eb9b79b7d8e758ea2aa611e0f
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2015-04-28 11:49:24 -0400

    Solution to Problem 9

---
+++ Euler'sProblem/src/Problem9.java
@@ -0,0 +1,14 @@
+/**
+ * Created by prince on 4/28/15.
+ */
+public class Problem9 {
+    public static void main(String[] args) {
+        for (int a = 1; a < 1000; a++) {
+            for (int b = a + 1; b < 1000; b++) {
+                int c = 1000 - a - b;
+                if (a * a + b * b == c * c)
+                    System.out.println(a*b*c);
+            }
+        }
+    }
+}
