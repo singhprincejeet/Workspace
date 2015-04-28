5c03248696b2d2987bc7143232dbafac293f5f6f
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2015-04-28 18:43:10 -0400

    Modifications to isPrime method

--- Euler'sProblem/src/Problem7.java
+++ Euler'sProblem/src/Problem7.java
@@ -1,29 +1,27 @@
 /**
  * Created by prince on 4/28/15.
  */
-public class Problem7 {
+    public class Problem7 {
     public static void main(String[] args) {
         int count = 0, result = 0;
         int i = 2;
         while (count < 10001) {
-            if (isPrime(i)){
+            if (isPrime(i)) {
                 count++;
-                result=i;
+                result = i;
             }
             i++;
         }
         System.out.println(result);
     }
 
-    private static boolean isPrime(long n)
-    {
+    private static boolean isPrime(long n) {
         boolean check = true;
-        int condition = (int)Math.sqrt(n);
-        for (int i=2; i<=  condition && check; i++)
-        {
+        for (int i = 2; i <= Math.sqrt(n) && check; i++) {
             check = !(n % i == 0);
         }
         return check;
     }
 }
+
 
