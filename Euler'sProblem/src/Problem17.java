a04e0e3bdb8753cdb8cd89afcf47a8942f522155
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2015-04-29 00:57:23 -0400

    Solution to Problem 17

---
+++ Euler'sProblem/src/Problem17.java
@@ -0,0 +1,13 @@
+/**
+ * Created by prince on 4/29/15.
+ */
+public class Problem17 {
+    public static void main(String[] args) {
+        int result = 3 + 3 + 5 +4 + 4 +3 +5 +5 +4;
+        result+=3 + 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8;
+        result+=10*(6 + 6 + 5 + 5 + 5 + 7 + 6 + 6) + 8*36;
+        result+=36*100+9*854+7*9+9*99*10+11;
+        System.out.print(result);
+    }
+
+}
