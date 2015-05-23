9ad763f88334ecee616fd0d14b00c5e9cb0cccb4
singhprincejeet <singhprincejeet@gmail.com>
2015-05-23 18:28:08 -0400

    Create Sudoku.py
    Sudoku [http://en.wikipedia.org/wiki/Sudoku]
    is a logic puzzle where a game
    is defined by a partially filled
    9 x 9 square of digits where each square
    contains one of the digits 1,2,3,4,5,6,7,8,9.
    For this question we will generalize
    and simplify the game.
    
    A valid sudoku square satisfies these
    two properties:
    
      1. Each column of the square contains
          each of the whole numbers from 1 to n exactly once.
    
      2. Each row of the square contains each
          of the whole numbers from 1 to n exactly once.

---
+++ Programs/Sudoku.py
@@ -0,0 +1,69 @@
+
+correct = [[1,2,3],
+           [2,3,1],
+           [3,1,2]]
+
+incorrect = [[1,2,3,4],
+             [2,3,1,3],
+             [3,1,2,3],
+             [4,4,4,4]]
+
+incorrect2 = [[1,2,3,4],
+             [2,3,1,4],
+             [4,1,2,3],
+             [3,4,1,2]]
+
+incorrect3 = [[1,2,3,4,5],
+              [2,3,1,5,6],
+              [4,5,2,1,3],
+              [3,4,5,2,1],
+              [5,6,4,3,2]]
+
+incorrect4 = [['a','b','c'],
+              ['b','c','a'],
+              ['c','a','b']]
+
+incorrect5 = [ [1, 1.5],
+               [1.5, 1]]
+               
+def check_sudoku(inputPuzzle):
+    size = len(inputPuzzle)
+    count = 1
+    while count <= size:
+        i = 0
+        while i < size:
+            j = 0
+            row = 0
+            col = 0
+            while j < size:
+                if inputPuzzle[i][j] == count:
+                    row = row + 1
+                if inputPuzzle[j][i] == count:
+                    col = col + 1
+                j = j + 1
+            if row != 1 or col != 1:
+                return False
+            i = i + 1
+        count = count + 1
+    return True
+
+
+
+print check_sudoku(incorrect)
+#>>> False
+
+print check_sudoku(correct)
+#>>> True
+
+print check_sudoku(incorrect2)
+#>>> False
+
+print check_sudoku(incorrect3)
+#>>> False
+
+print check_sudoku(incorrect4)
+#>>> False
+
+print check_sudoku(incorrect5)
+#>>> False
+
