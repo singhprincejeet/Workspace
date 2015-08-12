219e39796f393561e8c33ea315e12db2c69c1f85
singhprincejeet <singhprincejeet@gmail.com>
2015-08-12 18:57:27 +0000

    Named according to nomenaclature and added new file

--- Programs/make_url.py
+++ Programs/make_url.py
@@ -1,22 +1,88 @@
-str = "Programme de bourses ontariennes etudes Trillium";
+str = "Hours and Locations";
 strSplit = str.split();
 new = []
 for a in strSplit:
     if a == 'and'or a =='And':
         continue
+    elif a == 'les':
+        continue
+    elif a == 'des':
+        continue
+    elif a == 'le':
+        continue
     elif a == 'et':
+        continue
+    elif a == 'en':
         continue
     elif a == 'du':
         continue
     elif a == 'de':
         continue
+    elif a == 'per':
+        continue
+    elif a == 'like':
+        continue
+    elif a == 'into':
+        continue
     elif a == 'with':
+        continue
+    elif a == 'for':
+        continue
+    elif a == 'to':
+        continue
+    elif a == 'on':
+        continue
+    elif a == 'of':
+        continue
+    elif a == 'or':
+        continue
+    elif a == 'while':
+        continue
+    elif a == 'off':
+        continue
+    elif a == 'by':
         continue
     elif a == 'avec':
         continue
+    elif a == 'donc':
+        continue
     elif a == 'a'or a =='A':
         continue
+    elif a == 'an'or a =='An':
+        continue
+    elif a == 'the'or a =='The':
+        continue
     elif a == 'la':
+        continue
+    elif a == 'then':
+        continue
+    elif a == 'in':
+        continue
+    elif a == 'via':
+        continue
+    elif a == 'as':
+        continue
+    elif a == 'un':
+        continue
+    elif a == 'une':
+        continue
+    elif a == 'ni':
+        continue
+    elif a == 'dans':
+        continue
+    elif a == 'car':
+        continue
+    elif a == 'sur':
+        continue
+    elif a == 'sous':
+        continue
+    elif a == 'ou':
+        continue
+    elif a == 'mais':
+        continue
+    elif a == 'sauf':
+        continue
+    elif a == 'but':
         continue
     else:
         new.append(a)
@@ -29,5 +95,5 @@ for a in array:
     else:
         new.append(a)
 s = ''.join(new)
-s = 'students/awards/' + s
+s = 'tutorials/' + s
 print(s)
