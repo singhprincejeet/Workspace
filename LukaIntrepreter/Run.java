b59912c994711cfeeb88db0d0af983139ea9b950
singhprincejeet <singhprincejeet@gmail.com>
2015-11-28 13:25:22 -0500

    Runs the application.

---
+++ LukaIntrepreter/Run.java
@@ -0,0 +1,17 @@
+
+/**
+ * Runs the application.
+ *
+ * @author Marcel Turcotte
+ */
+public class Run {
+
+    /**
+     * Starts the application by creating an Interpreter and a Viewer.
+     *
+     * @param args unused.
+     */
+    public static void main(String[] args) {
+        new Viewer();
+    }
+}
